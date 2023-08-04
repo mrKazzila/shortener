3713247a96a0e052fef446190ebed45c86f03b92
mrKazzila <mrkazzila@gmail.com>
2023-08-04 16:58:55 +0300

    feat: add main database config

---
+++ app/database.py
@@ -0,0 +1,28 @@
+from typing import AsyncGenerator
+
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.pool import NullPool
+
+from app.config import DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DRIVER
+
+DATABASE_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+Base = declarative_base()
+metadata = MetaData()
+
+engine = create_async_engine(
+    url=DATABASE_URL,
+    poolclass=NullPool,
+)
+
+async_session_maker = async_sessionmaker(
+    engine=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
