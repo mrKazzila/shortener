be6f154359f5e1f9082027af0f8f395ee98b8afe
mrKazzila <mrkazzila@gmail.com>
2023-08-04 17:00:33 +0300

    feat: add shortener pydantic schem

---
+++ app/shortener/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class UrlBase(BaseModel):
+    target_link: str
+
+
+class Url(UrlBase):
+    is_active: bool
+    clicks_count: int
+
+    class Config:
+        from_attributes = True
+
+
+class UrlInfo(Url):
+    url: str
+    admin_url: str
