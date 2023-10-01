ba694a3d7d068749dab0dc837be72c879eea36d2
mrKazzila <mrkazzila@gmail.com>
2023-10-02 00:25:33 +0300

    chore: update after adding repository

--- backend/app/shortener/keygen.py
+++ backend/app/shortener/keygen.py
@@ -4,19 +4,16 @@ import app.shortener.services as services
 from app.shortener.utils import generate_random_key
 
 
-async def create_unique_random_key(session: AsyncSession):
+async def create_unique_random_key() -> str:
     """
     Creates a unique random key.
-
-    Args:
-        session: An asyncio.AsyncSession object.
 
     Returns:
         A unique random key.
     """
     key = generate_random_key()
 
-    while await services.get_db_url_by_key(session=session, url_key=key):
+    while await services.get_active_long_url_by_key(key=key):
         key = generate_random_key()
 
     return key
