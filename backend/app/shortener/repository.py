28ba2bfe93b9a04b12b27a60b3d47cca7aa94e93
mrKazzila <mrkazzila@gmail.com>
2023-10-02 19:28:53 +0300

    refactor: remove some variables

--- backend/app/shortener/repository.py
+++ backend/app/shortener/repository.py
@@ -1,8 +1,9 @@
-from app.core.repository import SQLAlchemyRepository
-from app.shortener.models import Url
-from app.shortener.schemas import SUrl
 from sqlalchemy import update
+
+from app.core.repository import SQLAlchemyRepository
 from app.settings.database import async_session_maker
+from app.shortener.models import Url
+from app.shortener.schemas import SUrlInfo
 
 
 class ShortenerRepository(SQLAlchemyRepository):
@@ -10,26 +11,23 @@ class ShortenerRepository(SQLAlchemyRepository):
 
     @classmethod
     async def add_url(cls, data: dict):
-        result = await super().add_entity(data=data)
-        return result
+        return await super().add_entity(data=data)
 
     @classmethod
     async def get_url_by_id(cls, id_: int):
-        url_ = await super().find_by_id(model_id=id_)
-        return url_
+        return await super().find_by_id(model_id=id_)
 
     @classmethod
     async def get_active_long_url(cls, url_key: str):
-        query_result = await super().find_one_or_none(key=url_key, is_active=True)
-        return query_result
+        return await super().find_one_or_none(key=url_key, is_active=True)
 
     @classmethod
-    async def update_redirect_counter(cls, url_):
+    async def update_redirect_counter(cls, url_: SUrlInfo):
         async with async_session_maker() as session:
             statement = (
                 update(cls.model)
                 .filter_by(id=url_.id)
-                .values(clicks_count=url_.clicks_count+1)
+                .values(clicks_count=url_.clicks_count + 1)
             )
 
             await session.execute(statement)
