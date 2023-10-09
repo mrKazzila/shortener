44e3933d26896368d62e3c892be6c8418972ea90
mrKazzila <mrkazzila@gmail.com>
2023-10-10 00:55:30 +0300

    feat: add abc uow & uow

---
+++ backend/app/core/unit_of_work.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+
+from app.settings.database import async_session_maker
+from app.shortener.repository import ShortenerRepository
+
+DEFAULT_SESSION_FACTORY = async_session_maker
+
+
+class ABCUnitOfWork(ABC):
+    shortener_repo: ShortenerRepository
+
+    @abstractmethod
+    def __init__(self):
+        ...
+
+    @abstractmethod
+    async def __aenter__(self):
+        ...
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        ...
+
+    @abstractmethod
+    async def commit(self):
+        ...
+
+    @abstractmethod
+    async def rollback(self):
+        ...
+
+
+class UnitOfWork(ABCUnitOfWork):
+
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+        self.shortener_repo = ShortenerRepository(session=self.session)
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
