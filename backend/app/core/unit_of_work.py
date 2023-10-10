e55908f21e4900809b2c3fc6c57878ced0142dee
mrKazzila <mrkazzila@gmail.com>
2023-10-10 20:17:03 +0300

    feat: add lazy initialization for repo & session

--- backend/app/core/unit_of_work.py
+++ backend/app/core/unit_of_work.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.settings.database import async_session_maker
 from app.shortener.repository import ShortenerRepository
 
@@ -15,18 +17,18 @@ class ABCUnitOfWork(ABC):
 
     @abstractmethod
     async def __aenter__(self):
+        return self
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        await self.rollback()
+
+    @abstractmethod
+    async def commit(self) -> None:
         ...
 
     @abstractmethod
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        ...
-
-    @abstractmethod
-    async def commit(self):
-        ...
-
-    @abstractmethod
-    async def rollback(self):
+    async def rollback(self) -> None:
         ...
 
 
@@ -34,16 +36,30 @@ class UnitOfWork(ABCUnitOfWork):
     def __init__(self, session_factory=DEFAULT_SESSION_FACTORY) -> None:
         self.session_factory = session_factory
 
-    async def __aenter__(self):
-        self.session = self.session_factory()
-        self.shortener_repo = ShortenerRepository(session=self.session)
+        self._session = None
+        self._shortener_repo = None
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.session.rollback()
+    @property
+    def session(self) -> AsyncSession:
+        if not self._session:
+            self._session = self.session_factory()
+        return self._session
+
+    @property
+    def shortener_repo(self) -> ShortenerRepository:
+        if not self._shortener_repo:
+            self._shortener_repo = ShortenerRepository(session=self.session)
+        return self._shortener_repo
+
+    async def __aenter__(self) -> ABCUnitOfWork:
+        return await super().__aenter__()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        await super().__aexit__(exc_type, exc_val, exc_tb)
         await self.session.close()
 
-    async def commit(self):
+    async def commit(self) -> None:
         await self.session.commit()
 
-    async def rollback(self):
+    async def rollback(self) -> None:
         await self.session.rollback()
