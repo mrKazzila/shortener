d1af82721326fd18e4f279cc4fb5007e78fce7fd
mrKazzila <mrkazzila@gmail.com>
2023-10-16 18:11:57 +0300

    refactor: update style

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,4 +7,7 @@ pytest_plugins = [
 
 # issue: Unable to use pytest with cache
 # https://github.com/long2ice/fastapi-cache/issues/49
-mock.patch("fastapi_cache.decorator.cache", lambda *args, **kwargs: lambda f: f).start()
+mock.patch(
+    "fastapi_cache.decorator.cache",
+    lambda *args, **kwargs: lambda f: f,
+).start()
