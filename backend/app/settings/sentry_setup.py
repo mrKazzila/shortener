c2247ea9cfe966b377b5a571b31cc0e06962e818
mrKazzila <mrkazzila@gmail.com>
2023-10-16 22:06:11 +0300

    feat: run sentry only for DEV & PROD

--- backend/app/settings/sentry_setup.py
+++ backend/app/settings/sentry_setup.py
@@ -9,14 +9,15 @@ logger = logging.getLogger(__name__)
 
 def sentry_setup() -> None:
     """Sentry setup."""
-    logger.info('Start sentry setup')
     try:
-        sentry_sdk.init(
-            dsn=settings().SENTRY_URL,
-            traces_sample_rate=settings().TRACES_SAMPLE_RATE,
-            profiles_sample_rate=settings().PROFILES_SAMPLE_RATE,
-        )
-        logger.info('Sentry setup successfully ended')
+        if settings().MODE != 'TEST':
+            logger.info('Start sentry setup')
+            sentry_sdk.init(
+                dsn=settings().SENTRY_URL,
+                traces_sample_rate=settings().TRACES_SAMPLE_RATE,
+                profiles_sample_rate=settings().PROFILES_SAMPLE_RATE,
+            )
+            logger.info('Sentry setup successfully ended')
 
     except Exception as e:
         logger.error('Sentry setup with error: %(error)s', {'error': e})
