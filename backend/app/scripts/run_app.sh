7c50ea44f8a0dce67d79204851c6f27b3d7693ca
Ilya Kazakov <kazzilaservices@gmail.com>
2023-10-23 20:34:54 +0300

    Tmp/pr/fix docker (#16)
    * chore: update "titles"
    
    * feat: add comment for sqlalchemy.url
    
    * chore: add new params from env file
    
    * ci: add new service for migration & refactoring
    
    * ci: add new shell file for migration
    
    * chore: set _sqlalchemy_url option
    
    * ci: add shell file for alembic migration
    
    * ci: move alembic migration command to other file
    
    * ci: update makefile commands
    
    * style: refactor by linters

--- backend/app/scripts/run_app.sh
+++ backend/app/scripts/run_app.sh
@@ -2,9 +2,6 @@
 
 current_dir=$(pwd)
 echo "Current dir: $current_dir"
-
-alembic_result=$(alembic upgrade head)
-echo "Migration result: $alembic_result"
-
+echo "Run gunicorn"
 
 gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
