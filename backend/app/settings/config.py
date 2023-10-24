e07c34a5b9e7851384df861b2eaca1ab7e2400c3
mrKazzila <mrkazzila@gmail.com>
2023-10-24 11:26:48 +0300

    refactor: update settings

--- backend/app/settings/config.py
+++ backend/app/settings/config.py
@@ -1,54 +1,28 @@
 import logging
 from functools import lru_cache
-from pathlib import Path
 from sys import exit
 from typing import Annotated, Literal, cast
 
 from annotated_types import Ge, Le, MinLen
 from pydantic import HttpUrl, PostgresDsn, RedisDsn, SecretStr, ValidationError
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 logger = logging.getLogger(__name__)
 
 
-class ProjectBaseSettings(BaseSettings):
-    __ROOT_DIR_ID: int = 2
-    __env_file = Path(__file__).resolve().parents[__ROOT_DIR_ID].joinpath('env/.env')
-
-    model_config = SettingsConfigDict(
-        env_file=__env_file,
-    )
-
-
-class ProjectSettings(ProjectBaseSettings):
-    """Settings for project."""
-
-    APP_NAME: str
-    MODE: Literal['TEST', 'DEV', 'PROD']
-    BASE_URL: str
-
-    DOMAIN: str
-    DOMAIN_PORT: Annotated[int, Ge(1), Le(65_535)]
-
-    KEY_LENGTH: Annotated[int, Ge(3), Le(10)]
+class SentrySettings(BaseSettings):
+    """Settings for Sentry."""
 
     SENTRY_URL: HttpUrl
     TRACES_SAMPLE_RATE: float
     PROFILES_SAMPLE_RATE: float
 
 
-class DatabaseSettings(ProjectBaseSettings):
-    """Settings for DB."""
-
-    POSTGRES_VERSION: str
-    DB_PROTOCOL: str
-    DB_HOST: str
-    DB_PORT: cast(str, Annotated[int, Ge(1), Le(65_535)])
-    DB_NAME: str
-    DB_USER: str
-    DB_PASSWORD: Annotated[SecretStr, MinLen(8)]
+class RedisSettings(BaseSettings):
+    """Settings for Redis."""
 
     REDIS_VERSION: str
+
     REDIS_HOST: str
     REDIS_PORT: Annotated[int, Ge(1), Le(65_535)]
     REDIS_CACHE_TIME: Annotated[int, Ge(1)]
@@ -64,6 +38,19 @@ class DatabaseSettings(ProjectBaseSettings):
 
         return str(url_)
 
+
+class DatabaseSettings(BaseSettings):
+    """Settings for DB."""
+
+    POSTGRES_VERSION: str
+
+    DB_PROTOCOL: str
+    DB_HOST: str
+    DB_PORT: cast(str, Annotated[int, Ge(1), Le(65_535)])
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: Annotated[SecretStr, MinLen(8)]
+
     @property
     def dsn(self, protocol=None) -> PostgresDsn:
         protocol = protocol or self.DB_PROTOCOL
@@ -75,13 +62,25 @@ class DatabaseSettings(ProjectBaseSettings):
             port=self.DB_PORT,
             path=f'{self.DB_NAME}',
         )
-        print(f'TEST {url_=}')
 
         return str(url_)
 
 
-class Settings(ProjectSettings, DatabaseSettings):
-    """Main project settings."""
+class Settings(BaseSettings):
+    """Main settings for project."""
+
+    db: DatabaseSettings = DatabaseSettings()
+    redis: RedisSettings = RedisSettings()
+    sentry: SentrySettings = SentrySettings()
+
+    APP_NAME: str
+    MODE: Literal['TEST', 'DEV', 'PROD']
+
+    BASE_URL: str
+    DOMAIN: str
+    DOMAIN_PORT: Annotated[int, Ge(1), Le(65_535)]
+
+    KEY_LENGTH: Annotated[int, Ge(3), Le(10)]
 
 
 @lru_cache
