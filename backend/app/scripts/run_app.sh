a95655a8237ffe8c02f010150019f36d4fc9ee93
mrKazzila <mrkazzila@gmail.com>
2023-11-08 15:41:23 +0300

    chore: run command by MODE state & update permissions

--- backend/app/scripts/run_app.sh
+++ backend/app/scripts/run_app.sh
@@ -2,6 +2,15 @@
 
 current_dir=$(pwd)
 echo "Current dir: $current_dir"
-echo "Run gunicorn"
 
-gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+if [ "$MODE" == "DEV" ]; then
+    echo "Running uvicorn in DEV mode"
+    uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+
+elif [ "$MODE" == "PROD" ]; then
+    echo "Running gunicorn in PROD mode"
+    gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+
+else
+    echo "Unknown mode: $MODE. Please set MODE to DEV or PROD."
+fi
