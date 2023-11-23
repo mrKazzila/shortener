d6828001702ce34e699a3378c5227c97473db00f
mrKazzila <mrkazzila@gmail.com>
2023-11-24 00:37:29 +0300

    refactor: add service layer folder & split modules to implement & abc

--- backend/app/service_layer/unit_of_work.py
+++ backend/app/service_layer/unit_of_work.py
@@ -1,27 +1,8 @@
-from abc import ABC, abstractmethod
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.shortener.repository import ShortenerRepository
+from app.service_layer.abc_unit_of_work import ABCUnitOfWork
 from app.settings.database import async_session_maker
-from app.shortener.repository import ShortenerRepository
-
-
-class ABCUnitOfWork(ABC):
-    shortener_repo: ShortenerRepository
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
-        await self.rollback()
-
-    @abstractmethod
-    async def commit(self) -> None:
-        ...
-
-    @abstractmethod
-    async def rollback(self) -> None:
-        ...
 
 
 class UnitOfWork(ABCUnitOfWork):
