66e44de6e8116d9cf921fe779d9c8c6a6b70968d
mrKazzila <mrkazzila@gmail.com>
2023-12-28 19:53:27 +0300

    feat!: remove frontend & infra modules

--- frontend/src/components/ui/ShortenButton.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ShortenButton = ({ onClick }) => {
-  return <button className="shorten-button" onClick={onClick}>Cut url</button>;
-};
-
-export default ShortenButton;
