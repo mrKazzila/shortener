2b1c3b07f2c849eb6c3ec2bf9e568f157f0f0136
mrKazzila <mrkazzila@gmail.com>
2024-03-11 21:20:27 +0300

    refactor: add __all__

--- backend/app/api/router_setup.py
+++ backend/app/api/router_setup.py
@@ -4,17 +4,17 @@ from sys import exit
 from fastapi import FastAPI
 
 from app.api.healthcheck.routers import router as healthcheck_router
-from app.api.shortener.routers import router as shortener_router
+from app.api.urls.routers import router as urls_router
 
 logger = logging.getLogger(__name__)
 
 
 def routers_setup(app: FastAPI) -> None:
-    """Setup project routers."""
+    """Setup project api."""
     try:
-        logger.info('Start routers setup')
+        logger.info('Start api setup')
 
-        app.include_router(shortener_router)
+        app.include_router(urls_router)
         app.include_router(healthcheck_router)
 
         logger.info('Routers setup successfully ended')
